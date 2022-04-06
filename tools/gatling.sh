c1a278d268e84ccba12b6402f7e4053cc809a083
niranjan-ramesh <nra38@sfu.ca>
2022-04-05 23:56:17 -0700

    Added call gatling script with arguments

--- tools/gatling.sh
+++ tools/gatling.sh
@@ -6,16 +6,11 @@ set -o errexit
 if [[ $# -ne 2 ]]
 then
   echo "Usage: ${0} USER_COUNT SIM_NAME"
-  echo " Some available simulation names:"
-  echo "   ReadUserSim"
-  echo "   ReadMusicSim"
-  echo "   ReadBothVaryingSim"
   exit 1
 fi
 
-tmpf=$(mktemp)
 export CLUSTER_IP=`tools/getip.sh kubectl istio-system svc/istio-ingressgateway`
-USERS=${1} SIM_NAME=${2} /opt/gatling/bin/gatling.sh -s proj756.${2} > ${tmpf} &
+USERS=${1} SIM_NAME=${2} make -e -f k8s.mak 
 echo
 echo "Control-C to end output when you have seen enough."
 echo "To stop the Gatling job, enter"
@@ -23,4 +18,3 @@ echo "   $ kill -9 $!"
 echo "The Gatling job will continue running until it is stopped via kill -9 or"
 echo "the container is exited."
 echo
-tail -f ${tmpf}
